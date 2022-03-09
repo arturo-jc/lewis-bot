6d2906e2726e8959b649227352849be513483112
Arturo Javier-Castellanos <a.javier.castellanos@gmail.com>
2022-03-08 23:32:11 -0600

    refactor

--- extract.py
+++ extract.py
@@ -3,7 +3,7 @@ from pdfminer.high_level import extract_text
 sample_passage = 'When running the above example, you can stop the program by pressing ctrl+c at the same time. As you can see, these loop constructs serve different purposes. The for loop runs for a fixed amount of times, while the while loop runs until the loop condition changes. In this example, the condition is the boolean True which will never change, so it will run forever.'
 sample_sentences = sample_passage.split('.')
 
-def find_longest_tweet(index_start, sentence_list):
+def get_longest_tweet(index_start, sentence_list):
     tweet = None
     cutoff = len(sentence_list)
     while cutoff > index_start:
@@ -15,15 +15,23 @@ def find_longest_tweet(index_start, sentence_list):
             break
     return tweet
 
-text = extract_text('sample.pdf')
-lines = text.splitlines()
-tweets = []
-for line in lines:
-    sentence_list = line.split(".")
-    for i in range(len(sentence_list)):
-        tweet = find_longest_tweet(i, sentence_list)
-        if (len(tweet) > 0):
-            tweets.append(tweet)
+def extract_tweets(min_tweet_length, pdf):
+    text = extract_text(pdf)
+    tweets = []
+    lines = text.splitlines()
+    for line in lines:
+        sentence_list = line.split(".")
+        for i in range(len(sentence_list)):
+            tweet = get_longest_tweet(i, sentence_list)
+            if tweet and len(tweet) > min_tweet_length:
+                tweets.append(tweet)
+    return tweets
+
+
+tweets = extract_tweets(
+    min_tweet_length=80,
+    pdf='otpow.pdf'
+)
 
 with open('tweets.txt', 'w') as file:
     for count, tweet in enumerate(tweets):
